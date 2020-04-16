0b04ae870aa949d8142059647745f763480577be
macyrate <mcr810@live.com>
2020-04-16 19:23:45 +0800

    更新注释

--- Algorithm/1.两数之和.cs
+++ Algorithm/1.两数之和.cs
@@ -13,23 +13,23 @@ public class Solution
 {
     public int[] TwoSum(int[] nums, int target)
     {
-        Dictionary<int, int> dict = new Dictionary<int, int>();
+        Dictionary<int, int> dict = new Dictionary<int, int>();     //用哈希表存储数组中元素的值与下标
         for (int i = 0; i < nums.Length; i++)
         {
-            if (dict.ContainsKey(nums[i]))
+            if (dict.ContainsKey(nums[i]))                          //由于数组中元素的值可能重复以造成碰撞，需要处理
             {
-                if (nums[i] * 2 == target)
+                if (nums[i] * 2 == target)                          //若重复的数刚好是target的一半，则找到了答案
                     return new int[] { dict[nums[i]], i };
             }
             else
             {
-                dict.Add(nums[i], i);
-                if (dict.ContainsKey(target - nums[i]) &&
-                    dict[target - nums[i]] != i)
+                dict.Add(nums[i], i);                               //一般情况下，直接加入哈希表
+                if (dict.ContainsKey(target - nums[i]) &&           //寻找哈希表里有没有正好能加成target的
+                    dict[target - nums[i]] != i)                    //光有还不行，万一自己刚好是target的一半，会找到自己
                     return new int[] { dict[target - nums[i]], i };
             }
         }
-        return new int[2];
+        return new int[] { -1, -1 };                                //实在没有，则返回{ -1, -1 }
     }
 }
 // @lc code=end
