c9d198720036bf125844588378b76b5cca1eb146
macyrate <mcr810@live.com>
2020-04-17 17:08:09 +0800

    再缩短几行

--- Algorithm/11.盛最多水的容器.cs
+++ Algorithm/11.盛最多水的容器.cs
@@ -14,20 +14,14 @@ public class Solution
 {
     public int MaxArea(int[] height)
     {
-        int start = 0, end = 1;     //表示当前处理窗口的首尾
-        int max = 0, area = 0;
+        int start = 0, end = 1, max = 0;     //表示当前处理窗口的首尾
 
         while (start < height.Length - end)
         {
             //计算目前窗口的容积，更新最大容积记录
-            area = Math.Min(height[start], height[^end]) * (height.Length - end - start);
-            max = area > max ? area : max;
-
+            max = Math.Max(max, Math.Min(height[start], height[^end]) * (height.Length - end - start));
             //根据木桶效应，短的一边限制了容积，将它移动一下
-            if (height[start] < height[^end])
-                start++;
-            else
-                end++;
+            _ = height[start] < height[^end] ? start++ : end++;
         }
         return max;
     }
