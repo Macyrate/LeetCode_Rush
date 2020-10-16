cbf5ab2d68fb307643d76655a3d173fe3487979e
macyrate <mcr810@live.com>
2020-10-16 23:09:57 +0800

    292. Nim 游戏

---
+++ Algorithm/292.nim-游戏.cs
@@ -0,0 +1,11 @@
+/*
+ * @lc app=leetcode.cn id=292 lang=csharp
+ *
+ * [292] Nim 游戏
+ */
+
+// @lc code=start
+public class Solution {
+    public bool CanWinNim (int n) => (n % 4 != 0);
+}
+// @lc code=end
